044f8745f1120336799034ec9ef7fa9be43019b5
creeper5820 <cqq1960180796@163.com>
2026-03-14 00:48:24 +0800

    wip: adapt current lidar

--- launch/launch.py
+++ launch/launch.py
@@ -2,12 +2,16 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
 
+    config = [
+        FindPackageShare("rmcs_local_map"), "/config", "/local_map.yaml"
+    ]
     location = Node(
         package="rmcs_local_map",
         executable="local_map",
-        parameters=[[FindPackageShare("rmcs_local_map"), "/config", "/local_map.yaml"]],
+        parameters=[config],
         output="log",
     )
     launch = LaunchDescription()
--- src/util/string.hpp
+++ src/util/string.hpp
@@ -3,35 +3,35 @@
 namespace rmcs::string {
 
 constexpr auto world_link = "world_link";
-constexpr auto slam_link  = "lidar_init";
-constexpr auto robot_link = "lidar_link";
-constexpr auto map_link   = "map_link";
+constexpr auto slam_link = "lidar_init";
+constexpr auto robot_link = "base_link";
+constexpr auto map_link = "map_link";
 
 namespace slam {
-    constexpr auto reset_service_name         = "/rmcs_slam/reset";
-    constexpr auto save_map_service_name      = "/rmcs_slam/map_save";
-    constexpr auto switch_record_service_name = "/rmcs_slam/switch_record";
+constexpr auto reset_service_name = "/rmcs_slam/reset";
+constexpr auto save_map_service_name = "/rmcs_slam/map_save";
+constexpr auto switch_record_service_name = "/rmcs_slam/switch_record";
 
-    constexpr auto cloud_effected_world_topic = "/rmcs_slam/cloud_effected";
-    constexpr auto cloud_world_topic          = "/rmcs_slam/cloud_registered_world";
-    constexpr auto cloud_body_topic           = "/rmcs_slam/cloud_registered_body";
-    constexpr auto constructed_map_topic      = "/rmcs_slam/constructed_map";
-    constexpr auto pose_topic                 = "/rmcs_slam/pose";
-    constexpr auto odometry_topic             = "/rmcs_slam/odometry";
-    constexpr auto path_topic                 = "/rmcs_slam/path";
+constexpr auto cloud_effected_world_topic = "/rmcs_slam/cloud_effected";
+constexpr auto cloud_world_topic = "/rmcs_slam/cloud_registered_world";
+constexpr auto cloud_body_topic = "/rmcs_slam/cloud_registered_body";
+constexpr auto constructed_map_topic = "/rmcs_slam/constructed_map";
+constexpr auto pose_topic = "/rmcs_slam/pose";
+constexpr auto odometry_topic = "/rmcs_slam/odometry";
+constexpr auto path_topic = "/rmcs_slam/path";
 
-    constexpr auto scanning_conbination_topic     = "/rmcs_slam/scanning";
-    constexpr auto yaw_velocity_combination_topic = "/rmcs_slam/yaw_velocity";
-}
+constexpr auto scanning_conbination_topic = "/rmcs_slam/scanning";
+constexpr auto yaw_velocity_combination_topic = "/rmcs_slam/yaw_velocity";
+} // namespace slam
 
 namespace location {
-    constexpr auto initialize_service_name = "/rmcs_location/initialize";
-    constexpr auto update_side_service     = "/rmcs_location/update_side";
-    constexpr auto pose_topic_name         = "/rmcs_location/pose";
-}
+constexpr auto initialize_service_name = "/rmcs_location/initialize";
+constexpr auto update_side_service = "/rmcs_location/update_side";
+constexpr auto pose_topic_name = "/rmcs_location/pose";
+} // namespace location
 
 namespace obtacle {
-    constexpr auto obstacle_map_topic = "/rmcs_map/map/grid";
+constexpr auto obstacle_map_topic = "/rmcs_map/map/grid";
 }
 
-}
+} // namespace rmcs::string
